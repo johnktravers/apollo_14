c7a562c4f4d1c6845c39fc9d0b3452a88856bafd
John Travers <johnk.travers@gmail.com>
2019-10-11 09:55:19 -0600

    Add seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+neil = Astronaut.create(name: 'Neil Armstrong', age: 37, job: 'Commander')
+gail = Astronaut.create(name: 'Gail Hopper', age: 42, job: 'Lead Scientist')
+nathan = Astronaut.create(name: 'Nathan Baxter', age: 35, job: 'Psychologist')
+
+apollo = Mission.create(title: 'Apollo 14', time_in_space: 144)
+capricorn = Mission.create(title: 'Capricorn 4', time_in_space: 68)
+gemini = Mission.create(title: 'Gemini 7', time_in_space: 96)
+
+AstronautMission.create(astronaut_id: neil.id, mission_id: apollo.id)
+AstronautMission.create(astronaut_id: neil.id, mission_id: gemini.id)
+AstronautMission.create(astronaut_id: gail.id, mission_id: capricorn.id)
+AstronautMission.create(astronaut_id: gail.id, mission_id: gemini.id)
+AstronautMission.create(astronaut_id: nathan.id, mission_id: apollo.id)
