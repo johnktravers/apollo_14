07573dc85ff92afd5638b2b9076a308667cb01d0
John Travers <johnk.travers@gmail.com>
2019-10-11 10:21:38 -0600

    Add test for total_time_in_space method

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -21,4 +21,20 @@ describe Astronaut, type: :model do
       expect(Astronaut.average_age).to eq(38)
     end
   end
+
+  describe '#total_time_in_space' do
+    it 'can calculate the total time an astronaut has spent in space' do
+      neil = Astronaut.create(name: 'Neil Armstrong', age: 37, job: 'Commander')
+
+      apollo = Mission.create(title: 'Apollo 14', time_in_space: 144)
+      capricorn = Mission.create(title: 'Capricorn 4', time_in_space: 68)
+      gemini = Mission.create(title: 'Gemini 7', time_in_space: 96)
+
+      AstronautMission.create(astronaut_id: neil.id, mission_id: gemini.id)
+      AstronautMission.create(astronaut_id: neil.id, mission_id: apollo.id)
+      AstronautMission.create(astronaut_id: neil.id, mission_id: capricorn.id)
+
+      expect(neil.total_time_in_space).to eq(308)
+    end
+  end
 end
